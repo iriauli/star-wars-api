7ac8aa9ba21bba1b2d7529c805858755ecb8f0d7
iriauli <irakliiriauli@gmail.com>
2021-12-18 06:41:32 +0400

    update main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import styles from "./Main.module.scss";
 
 const Main = () => {
   return (
-    <div>
+    <div className={styles.main}>
+      <img src="/logo.png" width="200px" height="200px" alt="img" />
       <h1>The Star Wars API</h1>
+      <p>Fetching info about people, films, starships, vehicles, species and planets from Star Wars API.</p>
     </div>
   );
 };
